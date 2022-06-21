4be7bb87368ce80c18b2350c3c190521e075fe81
ifantasyw <58970521+ifantasyw@users.noreply.github.com>
2022-06-21 20:52:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,3 @@ sed -i "s/OpenWrt /$(date +%Y.%m.%d) by FANTASYW /g" package/lean/default-settin
 
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
-
-echo '修改banner'
-rm -rf package/base-files/files/etc/banner
-cp -f banner package/base-files/files/etc/
