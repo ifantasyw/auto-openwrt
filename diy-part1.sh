5ffe6f215ef8c02a993b7adbd1dbb7fccd0454cc
ifantasyw <58970521+ifantasyw@users.noreply.github.com>
2022-06-21 20:14:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.defaul
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.defaul
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.defaul
