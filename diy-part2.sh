64338db5936d8a8d99f7050e0ba3b57917187bf0
ifantasyw <58970521+ifantasyw@users.noreply.github.com>
2022-06-21 20:19:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,10 +12,15 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+
+echo '修改机器名称'
+sed -i 's/OpenWrt/FWLEDE/g' package/base-files/files/bin/config_generate
+
+echo '签名'
 sed -i "s/OpenWrt /$(date +%Y.%m.%d) by FANTASYW /g" package/lean/default-settings/files/zzz-default-settings
 
 echo '修改时区'
-sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+sed -i "s/'UTC'/'CST-8'\n set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 echo '修改banner'
 rm -rf package/base-files/files/etc/banner
