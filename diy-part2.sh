8cf1968c8723b747893d6d05a0487407fa73af71
ifantasyw <58970521+ifantasyw@users.noreply.github.com>
2022-06-23 04:43:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ echo '修改机器名称'
 sed -i 's/OpenWrt/FWLEDE/g' package/base-files/files/bin/config_generate
 
 echo '签名'
-sed -i "s/OpenWrt /$(date +%Y.%m.%d) by FANTASYW /g" package/lean/default-settings/files/zzz-default-settings
+sed -i ‘s/OpenWrt /$(date +%Y.%m.%d) by FANTASYW /g’ package/lean/default-settings/files/zzz-default-settings
 
 echo '修改时区'
-sed -i "s/'UTC'/'CST-8'\n set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
